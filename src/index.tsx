25798b14a14321c3db4af3ceada0d80ae955a650
deguchi <deguchik@gmail.com>
2021-03-25 10:02:30 +0900

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+function hello(name: string): string {
+    return `Hello, ${name}!`;
+}
+
+console.log(hello("TightlyTemplate"));
+
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById('app'));
